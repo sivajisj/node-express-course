9fc8c207d17a1a404c27194a2721e414732fd21b
Anantharajupeta gadidala <sivajigsivajig703@gmail.com>
2023-10-08 11:50:29 +0000

    edited middleware files

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -7,15 +7,19 @@ const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
+
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
-
+app.use('/api/v1',mainRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 //routes
-app.use('api/v1',mainRouter)
+
+
+
 const port = process.env.PORT || 3000;
 
 const start = async () => {
